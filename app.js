a578e26be8621f392d101040f33735e7f6b80af2
zafra89 <51385853+zafra89@users.noreply.github.com>
2020-02-09 22:04:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,8 @@
 window.onload = function() {
   var src = document.getElementById("cloneSource"),
-      dst = document.getElementById("textarea");
-      dst1 = document.getElementById("textarea1");
-      dst2 = document.getElementById("textarea2");
-      dst3 = document.getElementById("textarea3");
-      dst4 = document.getElementById("textarea4");
-      dst5 = document.getElementById("textarea5");
-      dst6 = document.getElementById("textarea6");
-      dst7 = document.getElementById("textarea7");
+      dst = document.querySelectorAll("textarea");
   src.addEventListener('input', function() {
-      dst.value = src.value;
-      dst1.value = src.value;
-      dst2.value = src.value;
-      dst3.value = src.value;
-      dst4.value = src.value;
-      dst5.value = src.value;
-      dst6.value = src.value;
-      dst7.value = src.value;
+    dst.forEach(typing => typing.value = src.value)
   });
 };
 
@@ -39,74 +25,21 @@ function searchFunction() {
 
 
   function sizeChange() {
-      const text = document.getElementById("textarea");
-      const text1 = document.getElementById("textarea1");
-      const text2 = document.getElementById("textarea2");
-      const text3 = document.getElementById("textarea3");
-      const text4 = document.getElementById("textarea4");
-      const text5 = document.getElementById("textarea5");
-      const text6 = document.getElementById("textarea6");
-      const text7 = document.getElementById("textarea7");
+      const text = document.querySelectorAll("textarea");
       const size = document.getElementById("select-size");
+      size.addEventListener('click', function() {
+        text.forEach(pixels => pixels.style.fontSize = size.value)
+      });
+    };
 
-      if (size.value == 20) {
-        text.style.fontSize = '20px';
-        text1.style.fontSize = '20px';
-        text2.style.fontSize = '20px';
-        text3.style.fontSize = '20px';
-        text4.style.fontSize = '20px';
-        text5.style.fontSize = '20px';
-        text6.style.fontSize = '20px';
-        text7.style.fontSize = '20px';
-      } else if (size.value == 25) {
-        text.style.fontSize = '25px';
-        text1.style.fontSize = '25px';
-        text2.style.fontSize = '25px';
-        text3.style.fontSize = '25px';
-        text4.style.fontSize = '25px';
-        text5.style.fontSize = '25px';
-        text6.style.fontSize = '25px';
-        text7.style.fontSize = '25px';
-      } else if (size.value == 30) {
-        text.style.fontSize = '30px';
-        text1.style.fontSize = '30px';
-        text2.style.fontSize = '30px';
-        text3.style.fontSize = '30px';
-        text4.style.fontSize = '30px';
-        text5.style.fontSize = '30px';
-        text6.style.fontSize = '30px';
-        text7.style.fontSize = '30px';
-      } else if (size.value == 35) {
-        text.style.fontSize = '35px';
-        text1.style.fontSize = '35px';
-        text2.style.fontSize = '35px';
-        text3.style.fontSize = '35px';
-        text4.style.fontSize = '35px';
-        text5.style.fontSize = '35px';
-        text6.style.fontSize = '35px';
-        text7.style.fontSize = '35px';
-      } else if (size.value == 40) {
-        text.style.fontSize = '40px';
-        text1.style.fontSize = '40px';
-        text2.style.fontSize = '40px';
-        text3.style.fontSize = '40px';
-        text4.style.fontSize = '40px';
-        text5.style.fontSize = '40px';
-        text6.style.fontSize = '40px';
-        text7.style.fontSize = '40px';
-      }
-  }
-
-window.onclick = function() {
-  document.getElementById('reset-button').addEventListener('click', clear);
-}
-
-function resetInputs() {
+const textareaDefaultValue = 'Then Came the night of the first falling star.';
+document.getElementById("reset-button").addEventListener("click", function(){
+  document.querySelectorAll("textarea").forEach(reset => reset.value = textareaDefaultValue);
   document.getElementById("search-bar").value='';
   document.getElementById("cloneSource").value='';
-  document.getElementById("select-size").value='20';
-  
+  document.getElementById("select-size").value='20px';
+
   searchFunction();
   sizeChange();
-}
+}); 
   
